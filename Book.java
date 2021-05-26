466882893fab3df5c5cbed32deb0121a8da46fae
Harry <harry.booth-beach@student.onslow.school.nz>
2021-05-27 11:35:45 +1200

    Added image to our book class

--- Book.java
+++ Book.java
@@ -1,4 +1,4 @@
-
+import ecs100.*;
 /**
  * Support class of Book
  * A book contains an id, name, author, quantity, image
@@ -14,17 +14,28 @@ public class Book
     private String author;
     private int quantity;
     private String image;
+    static final String DEFAULT_IMAGE = "book.png";
     
     /**
      * Constructor for objects of class Book
      */
-    public Book(int id, String nm, String auth, int qty)
+    public Book(int id, String nm, String auth, int qty, String img)
     {
         // initialise instance variables
         this.id = id;
         this.name = nm;
         this.author = auth;
         this.quantity = qty;
+        this.image = img;
+    }
+    
+    /**
+     * Constructor overloading
+     * Set default image to obj
+     */
+    public Book(int id, String nm, String author, int qty)
+    {
+        this(id, nm, author, qty, DEFAULT_IMAGE);
     }
     
     /**
@@ -65,5 +76,18 @@ public class Book
     public int getQuantity()
     {
         return this.quantity;
+    }
+    
+    /**
+     * Display our image on our GUI
+     */
+    public void displayBook()
+    {
+        double locX = 100;
+        double locY = 100;
+        final double WIDTH = 100;
+        final double HEIGHT = 100;
+        
+        UI.drawImage(this.image, locX, locY, WIDTH, HEIGHT);
     }
 }
